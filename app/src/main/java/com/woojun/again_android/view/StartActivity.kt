515fc44699570de532fc44be551d6ad59adb445c
JangWoojun <woojun0107@naver.com>
2024-06-22 20:57:55 +0900

    feat: StartActivity 로직 구현

--- app/src/main/java/com/woojun/again_android/view/StartActivity.kt
+++ app/src/main/java/com/woojun/again_android/view/StartActivity.kt
@@ -1,5 +1,6 @@
 package com.woojun.again_android.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.woojun.again_android.R
@@ -11,5 +12,14 @@ class StartActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityStartBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.apply {
+            signUpButton.setOnClickListener {
+                startActivity(Intent(this@StartActivity, SignUpActivity::class.java))
+            }
+            loginButton.setOnClickListener {
+                startActivity(Intent(this@StartActivity, LoginActivity::class.java))
+            }
+        }
     }
 }
