cc36d492f3302b06f6b45dbc82a31fdcb1910b86
JangWoojun <woojun0107@naver.com>
2024-06-22 21:25:36 +0900

    feat: SignUpActivity 로직 구현

--- app/src/main/java/com/woojun/again_android/view/SignUpActivity.kt
+++ app/src/main/java/com/woojun/again_android/view/SignUpActivity.kt
@@ -1,12 +1,49 @@
 package com.woojun.again_android.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
 import com.woojun.again_android.R
+import com.woojun.again_android.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignUpBinding
+    private var passwordToggle = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            backButton.setOnClickListener {
+                finish()
+            }
+
+            passwordVisibleButton.setOnClickListener {
+                if (!passwordToggle) {
+                    passwordInput.inputType = InputType.TYPE_CLASS_TEXT
+                    passwordVisibleButton.setImageResource(R.drawable.visible_icon)
+                    passwordToggle = !passwordToggle
+                    passwordInput.setSelection(passwordInput.text.length)
+                } else {
+                    passwordVisibleButton.setImageResource(R.drawable.invisible_icon)
+                    passwordInput.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+                    passwordToggle = !passwordToggle
+                    passwordInput.setSelection(passwordInput.text.length)
+                }
+            }
+
+            nextButton.setOnClickListener {
+                startActivity(
+                    Intent(this@SignUpActivity, HobbyActivity::class.java).apply {
+                        putExtra("id", binding.idinput.text.toString())
+                        putExtra("password", binding.passwordInput.text.toString())
+                    }
+                )
+            }
+        }
+
     }
 }
