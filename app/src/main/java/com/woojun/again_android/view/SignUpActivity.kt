126262c0acba6175a2bad60b9b289acc7260a524
JangWoojun <woojun0107@naver.com>
2024-06-22 20:53:35 +0900

    design: SignUpActivity 추가

---
+++ app/src/main/java/com/woojun/again_android/view/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.woojun.again_android.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.woojun.again_android.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
