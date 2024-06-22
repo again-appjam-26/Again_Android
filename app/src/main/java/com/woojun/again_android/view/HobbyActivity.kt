7f73b567df4abe592c6a83d477e15b80eea84ac1
JangWoojun <woojun0107@naver.com>
2024-06-22 21:24:11 +0900

    feat: HobbyActivity 추가

---
+++ app/src/main/java/com/woojun/again_android/view/HobbyActivity.kt
@@ -0,0 +1,12 @@
+package com.woojun.again_android.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.woojun.again_android.R
+
+class HobbyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hobby)
+    }
+}
