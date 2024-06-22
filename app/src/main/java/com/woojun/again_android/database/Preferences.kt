cd8f49cb80165e3128124efa2e6c392faceddb49
JangWoojun <woojun0107@naver.com>
2024-06-23 06:09:51 +0900

    feat: 저장 함수 추가

--- app/src/main/java/com/woojun/again_android/database/Preferences.kt
+++ app/src/main/java/com/woojun/again_android/database/Preferences.kt
@@ -23,4 +23,40 @@ object Preferences {
         editor.apply()
     }
 
+    fun saveTime(context: Context, time: Long) {
+        val sharedPreferences: SharedPreferences = context.getSharedPreferences("MyPreferences", Context.MODE_PRIVATE)
+        val editor: SharedPreferences.Editor = sharedPreferences.edit()
+        editor.putLong("time", time)
+        editor.apply()
+    }
+
+    fun loadTime(context: Context): Long {
+        val sharedPreferences: SharedPreferences = context.getSharedPreferences("MyPreferences", Context.MODE_PRIVATE)
+        return sharedPreferences.getLong("time", 0)
+    }
+
+    fun saveDate(context: Context, date: String) {
+        val sharedPreferences: SharedPreferences = context.getSharedPreferences("MyPreferences", Context.MODE_PRIVATE)
+        val editor: SharedPreferences.Editor = sharedPreferences.edit()
+        editor.putString("date", date)
+        editor.apply()
+    }
+
+    fun loadDate(context: Context): String? {
+        val sharedPreferences: SharedPreferences = context.getSharedPreferences("MyPreferences", Context.MODE_PRIVATE)
+        return sharedPreferences.getString("date", null)
+    }
+
+    fun saveAppTime(context: Context, time: Long) {
+        val sharedPreferences: SharedPreferences = context.getSharedPreferences("MyPreferences", Context.MODE_PRIVATE)
+        val editor: SharedPreferences.Editor = sharedPreferences.edit()
+        editor.putLong("app_time", time)
+        editor.apply()
+    }
+
+    fun loadAppTime(context: Context): Long {
+        val sharedPreferences: SharedPreferences = context.getSharedPreferences("MyPreferences", Context.MODE_PRIVATE)
+        return sharedPreferences.getLong("app_time", 0)
+    }
+
 }
