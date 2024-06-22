0fa589db2f13aca28b263cfdbf0a842f5b877931
JangWoojun <woojun0107@naver.com>
2024-06-23 06:11:48 +0900

    feat: FilterActivity 로직 구현

--- app/src/main/java/com/woojun/again_android/view/FilterActivity.kt
+++ app/src/main/java/com/woojun/again_android/view/FilterActivity.kt
@@ -1,5 +1,8 @@
 package com.woojun.again_android.view
 
+import android.app.usage.UsageStats
+import android.app.usage.UsageStatsManager
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
@@ -9,11 +12,16 @@ import com.woojun.again_android.MainActivity
 import com.woojun.again_android.adapter.AppAdapter
 import com.woojun.again_android.data.AppInfo
 import com.woojun.again_android.database.AppDatabase
+import com.woojun.again_android.database.Preferences.saveDate
+import com.woojun.again_android.database.Preferences.saveTime
 import com.woojun.again_android.databinding.ActivityFilterBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.concurrent.TimeUnit
 
 class FilterActivity : AppCompatActivity() {
     private lateinit var binding: ActivityFilterBinding
@@ -31,11 +39,16 @@ class FilterActivity : AppCompatActivity() {
         binding.filterList.layoutManager = LinearLayoutManager(this@FilterActivity)
 
         binding.nextButton.setOnClickListener {
+            val item = mainAdapter.getCheckedAppList()
+
             CoroutineScope(Dispatchers.IO).launch {
                 val appInfoDAO = AppDatabase.getDatabase(this@FilterActivity)?.appInfoDAO()
-                appInfoDAO?.insertAppInfoList(mainAdapter.getCheckedAppList())
+                appInfoDAO?.insertAppInfoList(item)
 
                 withContext(Dispatchers.Main) {
+                    saveTime(this@FilterActivity, timeCalculation(this@FilterActivity, item.map { it.packageName }))
+                    saveDate(this@FilterActivity, getDate())
+
                     startActivity(Intent(this@FilterActivity, MainActivity::class.java))
                     finishAffinity()
                 }
@@ -46,6 +59,37 @@ class FilterActivity : AppCompatActivity() {
             finish()
         }
     }
+
+    private fun getDate(): String {
+        val currentDate = LocalDate.now()
+        val formatter = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일")
+        return currentDate.format(formatter)
+    }
+
+    private fun timeCalculation(context: Context, packageNames: List<String>): Long {
+        val usageStatsManager = context.getSystemService(Context.USAGE_STATS_SERVICE) as UsageStatsManager
+
+        val endTime = System.currentTimeMillis()
+        val startTime = endTime - TimeUnit.HOURS.toMillis(24)
+
+        val usageStatsList: List<UsageStats> = usageStatsManager.queryUsageStats(
+            UsageStatsManager.INTERVAL_DAILY, startTime, endTime
+        )
+
+        var time: Long = 0
+
+        for (usageStats in usageStatsList) {
+            val packageName = usageStats.packageName
+            if (packageNames.contains(packageName)) {
+                val totalTimeInForeground = usageStats.totalTimeInForeground
+                val totalTimeInMinutes = TimeUnit.MILLISECONDS.toMinutes(totalTimeInForeground)
+                time+=totalTimeInMinutes
+            }
+        }
+
+        return time
+    }
+
 
     private fun getAppInfo(): List<AppInfo> {
         val packageManager: PackageManager = packageManager
